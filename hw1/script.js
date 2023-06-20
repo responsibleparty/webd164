753d49a1118cb60f94a5b9338c353a4a8b0c56b0
Cecilia Glynn <ceciliaglynn@icloud.com>
2023-06-20 10:23:42 -0700

    Homework #1

---
+++ hw1/script.js
@@ -0,0 +1,11 @@
+alert("Enter two numbers");
+
+let num1 = prompt("Enter a number");
+num1 = parseInt(num1);
+
+let num2 = prompt("Enter another number");
+num2 = parseInt(num2);
+
+let sum = num1 + num2;
+
+alert(`You entered ${num1} and ${num2}. Your sum is ${sum}`);
